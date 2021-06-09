7ca5eed46d1050e8d5e4d8395c70ad06a3b4bfe7
wesley-howell <wesley.howell@kellogg.northwestern.edu>
2021-06-09 18:36:58 -0500

    x

---
+++ censusdata.js
@@ -0,0 +1,16 @@
+// write code to read csv files
+let csv = require('neat-csv')
+
+// write code to read files from disk (necessary here?)
+let fs= require('fs')
+
+// define lambda function
+exports.handler = async function(event) {
+
+    // write event object to back-end console, to examine
+    console.log(event)
+
+    // get relevant output variables from queryStringParameters
+
+
+}
