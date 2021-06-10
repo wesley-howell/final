c2117724450370c736b3469993fa141ef00a1982
wesley-howell <wesley.howell@kellogg.northwestern.edu>
2021-06-10 09:48:41 -0500

    fmlx2

--- netlify/functions/create_search.js
+++ netlify/functions/create_search.js
@@ -12,11 +12,21 @@ exports.handler = async function(event) {
      
 
     // get the necessary query string parameters and store in memory
+    let userName = event.queryStringParameters.userName
+    let stateId = event.queryStringParameters.state_id
+    let city = event.queryStringParameters.city 
+
 
     // establish a connection to firebase in memory
+    let db = firebase.firestore()
 
-
-
+    // create new post to firestore db for new user search
+    await db.collection(`searches`).add({
+        userName: userName,
+        state: stateId,
+        city: city,
+        created: firebase.firestore.FieldValue.serverTimestamp()
+    })
 
     return {
         statusCode: 200
